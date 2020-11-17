519690c3361cce4ea3a359a93ef14e4717fbfb36
TheJonu <the.jonu@gmail.com>
2020-11-17 11:52:47 +0100

    komendy sql

---
+++ komendy.sql
@@ -0,0 +1,7 @@
+INSERT INTO "Kluby" VALUES (0, 'Jukan', '2010-06-20', 'Antoni Juk', '0000', NULL);
+INSERT INTO "Kluby" VALUES (1, 'Pukam', '2010-02-19', 'Bartosz Puk', '0001', NULL);
+
+TRUNCATE TABLE "Kluby";
+
+SELECT * FROM "Kluby";
+
